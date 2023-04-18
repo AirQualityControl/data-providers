6fcddf722a0b5340e0c4b53a935aaf408ee2c7a2
Vladyslav Kapliuk <kuplykvlad@gmail.com>
2023-04-19 00:41:37 +0300

    Adjusted Script to install local debug dotnet tool

--- scripts/CreateNewDataProvider.bat
+++ scripts/CreateNewDataProvider.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set /p project_name=Enter project name like example AirSnitch.DataProviders.MyAwesomeProvier 
+set /p project_name=Enter project name example AirSnitch.DataProviders.EcoCity 
 
 set "dotnet_lambda_tools="
 for /f "tokens=*" %%a in ('dotnet tool list --global ^| findstr /i Amazon.Lambda.Tools') do set "dotnet_lambda_tools=%%a"
@@ -8,6 +8,7 @@ for /f "tokens=*" %%a in ('dotnet tool list --global ^| findstr /i Amazon.Lambda
 if "%dotnet_lambda_tools%"=="" (
   dotnet tool install -g Amazon.Lambda.Tools
   dotnet new --install Amazon.Lambda.Templtes
+  dotnet tool install --global Amazon.Lambda.TestTool-7.0
 ) 
 cd ..
 cd src
