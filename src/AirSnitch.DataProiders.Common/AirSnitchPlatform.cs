0baf630bfa3861e711e7789a830314e139d3e3eb
Vladyslav Kapliuk <v.kapliuk@creatio.com>
2023-12-02 21:17:21 +0200

    Applied changes to support reqular SQS queue not a fifo

--- src/AirSnitch.DataProiders.Common/AirSnitchPlatform.cs
+++ src/AirSnitch.DataProiders.Common/AirSnitchPlatform.cs
@@ -8,7 +8,6 @@ namespace AirSnitch.DataProiders.Common;
 public class AirSnitchPlatform
 {
 
-	private static readonly Guid MessageGroupId = Guid.Parse("012f6009-4e6e-455f-9b9b-98f2dc9a6155");
 	public static async Task SubmitMeasurement(DataPoint dataPoint, SqsConfig config) {
 #if DEBUG
 		await Task.Delay(400);
@@ -17,8 +16,6 @@ public class AirSnitchPlatform
 		var amazonSqsConfig = new AmazonSQSConfig();
 		var client = new AmazonSQSClient(awsCreds, amazonSqsConfig);
 		var messageRequest = new SendMessageRequest(queueUrl: config.QueueUrl, messageBody: dataPoint.Serialize());
-		messageRequest.MessageGroupId = MessageGroupId.ToString();
-        messageRequest.MessageDeduplicationId = Guid.NewGuid().ToString(); //Skip message deduplication. Treat each message as a unique one
         await client.SendMessageAsync(messageRequest);
 #endif
     }
